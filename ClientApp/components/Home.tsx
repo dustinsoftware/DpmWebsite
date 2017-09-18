95667d0165b2c06d8c395fbd5197f5e605f98b43
Dustin Masters <dustin.masters@faithlife.com>
2017-09-17 21:30:24 -0700

    Add github

--- ClientApp/components/Home.tsx
+++ ClientApp/components/Home.tsx
@@ -21,7 +21,7 @@ export class Home extends React.Component<RouteComponentProps<{}>, {}> {
     public render() {
         return <div>
             <h1>Dustin Software, Inc.</h1>
-            <h4><a href="//twitter.com/dustinsoftware">Twitter</a> &middot; <a href="https://stackoverflow.com/cv/dustinmasters">Stack Overflow Careers</a></h4>
+            <h4><a href="//twitter.com/dustinsoftware">Twitter</a> &middot; <a href="https://github.com/dustinsoftware">Github</a>&middot; <a href="https://stackoverflow.com/cv/dustinmasters">Careers</a></h4>
             <p>Hi there! I build software. This page is running on a Raspberry PI 3, using Docker and ASP.NET Core. I use Twitter occasionally, you can follow me <a href="//twitter.com/dustinsoftware">@dustinsoftware</a>. This website is a playground for different experiments.</p>
             <h4>It looks like you are currently {this.state.isOnline ? 'online 🌞' : 'offline! 🌚'}</h4>
             <p>This uses a service worker to cache pages and assets locally. Turn off your wifi and reload this page!</p>
